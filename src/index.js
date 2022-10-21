5ad19e7fecd4f62389e2e28dc7428a5d2a1d3113
txy <1832330013@qq.com>
2022-10-21 15:12:52 +0800

    基本结构

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Str from "./str";
+const app = document.getElementById("app");
+const h3 = `<h3>哈哈哈哈测试</h3>`;
+app.innerHTML = Str.link + h3;
+// console.log(Str.link + h3);
---
+++ src/str.js
@@ -0,0 +1,3 @@
+export default {
+    link: '<a href="http://www.baidi.com">百度AAA</a>'
+}
